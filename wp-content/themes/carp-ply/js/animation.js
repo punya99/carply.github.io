5ba4e8277f0f707927f4995f208a63f60d857f7d
Punya <iampunya98@gmail.com>
2022-10-06 11:09:24 +0530

    aa

---
+++ wp-content/themes/carp-ply/js/animation.js
@@ -0,0 +1,42 @@
+var getElementsInArea = (function(docElm){
+    var viewportHeight = docElm.clientHeight;
+
+    return function(e, opts){
+        var found = [], i;
+        
+        if( e && e.type == 'resize' )
+            viewportHeight = docElm.clientHeight;
+
+        for( i = opts.elements.length; i--; ){
+            var elm        = opts.elements[i],
+                pos        = elm.getBoundingClientRect(),
+                topPerc    = pos.top    / viewportHeight * 100,
+                bottomPerc = pos.bottom / viewportHeight * 100,
+                middle     = (topPerc + bottomPerc)/2,
+                inViewport = middle > opts.zone[1] && 
+                             middle < (100-opts.zone[1]);
+
+            elm.classList.toggle(opts.markedClass, inViewport);
+
+            if( inViewport )
+                found.push(elm);
+        }
+    };
+})(document.documentElement);
+
+
+////////////////////////////////////
+// How to use:
+
+window.addEventListener('scroll', f)
+window.addEventListener('resize', f)
+
+function f(e){
+
+    
+    getElementsInArea(e, {
+        elements    : document.querySelectorAll('.ani_mation'), 
+        markedClass : 'highlight--2',
+        zone        : [10, 10] // percentage distance from top & bottom
+    });
+}
